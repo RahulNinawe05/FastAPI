c64da46381e4007716c434fcc5063f80acf0f3a4
Rahul Ninawe <ninawerahul818@gmail.com>
2026-02-03 19:28:34 +0530

    add paginated for practice

--- app_01.py
+++ app_01.py
@@ -1,5 +1,5 @@
-# Run - uvicorn main:app --reload
-from fastapi import FastAPI,Path
+# Run - uvicorn app_01:app --reload
+from fastapi import FastAPI,Path, HTTPException,Query
 import json
 app = FastAPI()
 
@@ -18,12 +18,52 @@ def Show():
 
     return data
 
-@app.get("/Cars/{Cars_id}")
-def Show_Cars_Data(Cars_id: int = Path(..., description="ID Present in ShowRoom Database", example="1", ge=1, le=10)):
+@app.get("/cars/{cars_id}")
+def Show_Cars_Data(cars_id: int = Path(..., description="ID Present in ShowRoom Database", example="1", ge=1, le=10)):
     data = Load_data()
 
     for car in data:
-        if car['id'] == Cars_id:
+        if car['id'] == cars_id:
             return car
     
-    return {"error": "cars data not found"}
+    raise HTTPException(status_code=404, detail="cars data not found")
+
+@app.get("/sort")
+def sorting_data(
+    sort_by: str = Query(..., description="Sorting By 'brand','model','year','transmission"),
+    order_by: str = Query("asc",description="The Order Sorting is 'asc','desc'"),
+    page: int = Query(1,ge=1),
+    limit: int = Query(2, ge=2, le=50)
+):
+    valid_field = ['brand', 'model', 'year', 'transmission']
+
+    if sort_by not in valid_field:
+        raise HTTPException(status_code=400, detail=f"Invalid Field {valid_field}")
+    
+    if order_by not in ['asc', 'desc']:
+        raise HTTPException(status_code=400,detail="Invlid Order, Select 'asc' OR 'desc' ")
+    
+    data = Load_data()
+
+    sort_order  = True if order_by== 'desc' else False
+    
+    sorted_data = sorted(
+        data, 
+        key = lambda x: str(x.get(sort_by, "")).lower(), 
+        reverse= sort_order
+        )
+    
+    start = (page - 1) * limit
+
+    end = start + limit
+
+    paginated_data = sorted_data[start:end]
+
+    return {
+        "page": page,
+        "limit": limit,
+        "total": len(sorted_data),
+        "data": paginated_data
+    }
+
+# EX.- "http://127.0.0.1:8000/sort?sort_by=brand&order_by=desc"
